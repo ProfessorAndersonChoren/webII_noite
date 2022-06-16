9fd228a9ffcc5c04802930c369a2441b8c0b3bd1
Professor Anderson Choren <anderson.choren@qi.edu.br>
2022-06-15 21:56:08 -0300

    Adição do controlador de produtos

---
+++ loja_de_varejo/controller/Product.php
@@ -0,0 +1,9 @@
+<?php
+$productName = $_POST["name"];
+$productPrice = $_POST["price"];
+$quantity = $_POST["quantity"];
+
+echo "<h1>Dados do produto</h1>";
+echo "<p>Nome do produto: $productName</p>";
+echo "<p>Preço do produto: $productPrice</p>";
+echo "<p>Quantidade em estoque: $quantity</p>";
--- loja_de_varejo/view/form_add_product.php
+++ loja_de_varejo/view/form_add_product.php
@@ -16,17 +16,20 @@
             <section class="columns-3">
                 <article>
                     <label for="name">Nome do produto</label>
-                    <input type="text" id="name" name="name" class="border border-blue-400">
+                    <input type="text" id="name" name="name" class="border border-blue-400" required minlength="5">
                 </article>
                 <article>
                     <label for="price">Preço</label>
-                    <input type="number" id="price" name="price" class="border border-blue-400">
+                    <input type="text" id="price" name="price" class="border border-blue-400" required min="1" max="1000">
                 </article>
                 <article>
                     <label for="quantity">Quantidade em estoque</label>
-                    <input type="number" id="quantity" name="quantity" class="border border-blue-400">
+                    <input type="number" id="quantity" name="quantity" class="border border-blue-400" required min="1" max="1000">
                 </article>
             </section>
+            <article class="flex justify-center mt-4">
+                <button type="submit" class="p-4 text-white bg-blue-700 rounded">Cadastrar</button>
+            </article>
         </fieldset>
     </form>
 </body>
