ff204dde6c6880adeaa5cdf5853ce0535146781a
Professor Anderson Choren <anderson.choren@qi.edu.br>
2022-07-19 21:51:28 -0300

    Remoção de produtos

--- loja_de_varejo/src/Controller/Product.php
+++ loja_de_varejo/src/Controller/Product.php
@@ -22,6 +22,9 @@ switch ($_GET['operation']) {
     case 'list':
         listProducts();
         break;
+    case 'remove':
+        removeProduct();
+        break;
     default:
         Redirect::redirect(message: 'A operação informada é inválida!!!', type: 'error');
 }
@@ -111,5 +114,35 @@ function listProducts()
         }
     } catch (PDOException $e) {
         Redirect::redirect("Lamento, houve um erro inesperado!!!", type: 'error');
+    }
+}
+
+function removeProduct()
+{
+    if (empty($_GET['code'])) {
+        Redirect::redirect(message: 'O código do produto não foi informado!!!', type: 'error');
+    }
+
+    $code = (float) $_GET['code'];
+    $error = array();
+
+    if (!Validation::validateNumber($code)) {
+        array_push($error, 'Código do produto inválido!!!');
+    }
+
+    if ($error) {
+        Redirect::redirect($error, type: 'warning');
+    } else {
+        try {
+            $dao = new ProductDAO();
+            $result = $dao->delete($code);
+            if ($result) {
+                Redirect::redirect(message: 'Produto removido com sucesso!!!');
+            } else {
+                Redirect::redirect(message: ['Não foi possível remover o produto'], type: 'warning');
+            }
+        } catch (PDOException $e) {
+            Redirect::redirect("Lamento, houve um erro inesperado!!!", type: 'error');
+        }
     }
 }
